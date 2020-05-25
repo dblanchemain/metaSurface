feba4d161852636f68f6b2b39a419a3c46a93150
dblanchemain <57360484+dblanchemain@users.noreply.github.com>
2020-05-25 23:13:29 +0200

    Update metaSurface.sh

--- metaSurface.sh
+++ metaSurface.sh
@@ -1,18 +1,18 @@
 #! /bin/sh
 
-dir="${HOME}/metaSurface4"
+dir="${HOME}/metaSurface"
 if [ -d ${dir} ] ; then
     echo "Le dossier paramètres ${dir} existe"
 else    
 	mkdir ${dir}
 	echo "Le dossier paramètres ${dir} est créé"
-	cp  ~/Compile/CodeBlocks/metaSurfaces3/parametres.conf ${dir}	
+	cp  parametres.conf ${dir}	
 fi
-dir="${HOME}/metaSurface2/surfaces"
+dir="${HOME}/metaSurface/surfaces"
 if [ -d ${dir} ] ; then
     echo "Le dossier de travail ${dir} existe"
 else    
 	mkdir ${dir}
 	echo "Le dossier de travail ${dir} est créé"
 fi
-~/Compile/CodeBlocks/metaSurface4/metaSurface.bin "${HOME}/Compile/CodeBlocks/metaSurface4" "${HOME}"
+./metaSurface.bin "${HOME}/metaSurface" "${HOME}"
